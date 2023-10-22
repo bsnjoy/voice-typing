c5ad9a41c7a36a336f05d23dc00f2c302ff1484b
s-Macbook-Air-m1 <bsnjoy@gmail.com>
2023-10-22 14:26:39 +0700

    Change primary_mic_name, backup_mic_name to priority_mic_names. So now can use unlimited number of mic names

--- select_mic.py
+++ select_mic.py
@@ -7,20 +7,18 @@ import json
 def select_mic():
     """
     Returns the index and name of the desired microphone in JSON format based on availability.
-    Prioritize config.primary_mic_name, if not available, then use config.backup_mic_name if available.
-    If both not available, fallback to config.fallback_mic_index
+    Prioritize the microphones in config.priority_mic_names in order, if available.
+    If none are available, fallback to config.fallback_mic_index
     """
     mic_info = {"index": config.fallback_mic_index, "name": "Default"}
     try:
         devices = sd.query_devices()
 
-        for device in devices:
-            if device['name'] == config.primary_mic_name:
-                mic_info = {"index": device["index"], "name": device["name"]}
-                break
-
-            if device['name'] == config.backup_mic_name:
-                mic_info = {"index": device["index"], "name": device["name"]}
+        for preferred_mic in config.priority_mic_names:
+            for device in devices:
+                if device['name'] == preferred_mic:
+                    mic_info = {"index": device["index"], "name": device["name"]}
+                    return {"status": "success", **mic_info}
 
         return {"status": "success", **mic_info}
 
